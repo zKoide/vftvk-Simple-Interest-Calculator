2af24cf889ff281753cc80352b37dada4ae86314
zKoide <alex12345abcdef@hotmail.com>
2021-05-26 22:45:41 +0000

    Atualizado css, script e front

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function compute(){
     }
     //case true show result
     else{
-        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+        document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%\</mark><br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
     }
 }
 //update rate value
